86f2a50620980d5ece55d7ee4bf721dcf1c1e9f8
Norbert Podsadowski <norbo11@googlemail.com>
2019-02-05 13:08:57 +0000

    Request simulator

---
+++ request_simulator.py
@@ -0,0 +1,39 @@
+import requests
+import random
+import time
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+
+BASE_PATH = 'http://localhost:8080/norbert6/playground-application/0.0.1/'
+
+
+def main():
+    logger.info('Starting requests simulator')
+
+    while True:
+        i = random.randint(1, 3)
+        endpoint = ''
+
+        if i == 1:
+            endpoint = 'one'
+
+        if i == 2:
+            endpoint = 'two'
+
+        if i == 3:
+            endpoint = 'three'
+
+        endpoint = BASE_PATH + endpoint
+
+        logger.info(f'Sending requests to {endpoint}')
+        requests.get(endpoint)
+
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
