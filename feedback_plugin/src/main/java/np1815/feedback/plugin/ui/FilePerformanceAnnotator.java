9a3a42191803d49ccd3962d3bbb666e80779fa14
Norbert Podsadowski <norbo11@googlemail.com>
2019-04-24 22:51:43 +0100

    Add annotator file in case it comes useful later

---
+++ feedback_plugin/src/main/java/np1815/feedback/plugin/ui/FilePerformanceAnnotator.java
@@ -0,0 +1,13 @@
+package np1815.feedback.plugin.ui;
+
+import com.intellij.lang.annotation.AnnotationHolder;
+import com.intellij.lang.annotation.Annotator;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
+
+public class FilePerformanceAnnotator implements Annotator {
+    @Override
+    public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
+    }
+
+}
