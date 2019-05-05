3a14ee737ba521b886d846929dd69a602918df91
Norbert Podsadowski <norbo11@googlemail.com>
2019-05-05 02:13:52 +0100

    Use GUI designer for feedback config panel

--- feedback_plugin/src/main/java/np1815/feedback/plugin/ui/FeedbackConfigurationPanel.java
+++ feedback_plugin/src/main/java/np1815/feedback/plugin/ui/FeedbackConfigurationPanel.java
@@ -13,51 +13,42 @@ import java.awt.event.ItemEvent;
 
 public class FeedbackConfigurationPanel {
 
-    private final JTextField metricBackendUrlTextfield;
-    private final JTextField feedbackConfigPathTextField;
-    private final JCheckBox takeBackendUrlFromConfigCheckbox;
-    private final BorderLayoutPanel rootComponent;
+    private final Project project;
     private final FeedbackDrivenDevelopment feedbackComponent;
 
+    private JPanel rootComponent;
+
+    /* Bound to GUI Designer */
+    private JCheckBox takeBackendUrlFromConfigCheckBox;
+    private JTextField metricBackendUrlTextField;
+
+    /* Custom initialization */
+    private JTextField feedbackConfigPathTextField;
+    private TextFieldWithBrowseButton chooseDirectoriesToInstrumentBrowser;
+
     public FeedbackConfigurationPanel(Project project, FeedbackDrivenDevelopment feedbackComponent) {
+        this.project = project;
         this.feedbackComponent = feedbackComponent;
 
-        metricBackendUrlTextfield = new JTextField();
-        feedbackConfigPathTextField = new JTextField();
-        takeBackendUrlFromConfigCheckbox = new JCheckBox("Take from config");
-
-        FileChooserDescriptor fileChooserDescriptor = new FileChooserDescriptor(true, false, false, false, false, false);
-        TextFieldWithBrowseButton chooseDirectoriesToInstrumentBrowser = new TextFieldWithBrowseButton(feedbackConfigPathTextField);
-        chooseDirectoriesToInstrumentBrowser.addBrowseFolderListener(new TextBrowseFolderListener(fileChooserDescriptor, project));
-
-        takeBackendUrlFromConfigCheckbox.addItemListener(a -> metricBackendUrlTextfield.setEnabled(a.getStateChange() == ItemEvent.DESELECTED));
-
-        /* Set up layout */
-        rootComponent = new BorderLayoutPanel(0, 2);
-
-        BorderLayoutPanel metricBackendUrlPanel = new BorderLayoutPanel(2, 0);
-        metricBackendUrlPanel.addToLeft(new Label("Metric Backend Url"));
-        metricBackendUrlPanel.addToCenter(metricBackendUrlTextfield);
-        metricBackendUrlPanel.addToRight(takeBackendUrlFromConfigCheckbox);
-
-        BorderLayoutPanel chooseDirectoriesToInstrumentPanel = new BorderLayoutPanel();
-        chooseDirectoriesToInstrumentPanel.addToLeft(new JLabel("Feedback Config Path"));
-        chooseDirectoriesToInstrumentPanel.addToCenter(chooseDirectoriesToInstrumentBrowser);
-
-        rootComponent.addToTop(metricBackendUrlPanel);
-        rootComponent.addToCenter(chooseDirectoriesToInstrumentPanel);
-        rootComponent.setPreferredSize(new Dimension(400, 500));
+        takeBackendUrlFromConfigCheckBox.addItemListener(a -> metricBackendUrlTextField.setEnabled(a.getStateChange() == ItemEvent.DESELECTED));
 
         update();
     }
 
     public void update() {
-        metricBackendUrlTextfield.setText(feedbackComponent.getState().metricBackendUrl);
+        metricBackendUrlTextField.setText(feedbackComponent.getState().metricBackendUrl);
         feedbackConfigPathTextField.setText(feedbackComponent.getState().feedbackConfigPath);
-        takeBackendUrlFromConfigCheckbox.setSelected(feedbackComponent.getState().takeMetricBackendUrlFromConfig);
+        takeBackendUrlFromConfigCheckBox.setSelected(feedbackComponent.getState().takeMetricBackendUrlFromConfig);
     }
 
-    public BorderLayoutPanel getRootComponent() {
+    public JPanel getRootComponent() {
         return rootComponent;
+    }
+
+    private void createUIComponents() {
+        FileChooserDescriptor fileChooserDescriptor = new FileChooserDescriptor(true, false, false, false, false, false);
+        feedbackConfigPathTextField = new JTextField();
+        chooseDirectoriesToInstrumentBrowser = new TextFieldWithBrowseButton(feedbackConfigPathTextField);
+        chooseDirectoriesToInstrumentBrowser.addBrowseFolderListener(new TextBrowseFolderListener(fileChooserDescriptor, project));
     }
 }
