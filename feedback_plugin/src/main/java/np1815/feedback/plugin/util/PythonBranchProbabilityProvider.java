4385d57d8fbd12ea4e046a793c37b729291a273b
Norbert Podsadowski <norbo11@googlemail.com>
2019-05-08 18:11:40 +0100

    Improve branch probabilities by scanning over lines in a smarter way

--- feedback_plugin/src/main/java/np1815/feedback/plugin/util/PythonBranchProbabilityProvider.java
+++ feedback_plugin/src/main/java/np1815/feedback/plugin/util/PythonBranchProbabilityProvider.java
@@ -2,21 +2,21 @@ package np1815.feedback.plugin.util;
 
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
 import com.intellij.psi.util.PsiTreeUtil;
-import com.jetbrains.python.PyElementTypes;
 import com.jetbrains.python.psi.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 
 public class PythonBranchProbabilityProvider implements BranchProbabilityProvider {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PythonBranchProbabilityProvider.class);
 
     private final PsiManager psiManager;
     private final FileDocumentManager fileDocumentManager;
@@ -38,57 +38,82 @@ public class PythonBranchProbabilityProvider implements BranchProbabilityProvide
         assert document != null;
 
         Map<Integer, Double> executionProbabilities = new HashMap<Integer, Double>();
-        Map<Integer, Integer> ifStatementTotalCount = new HashMap<Integer, Integer>();
 
         pyFile.accept(new PyRecursiveElementVisitor() {
 
+            // For every if statement, calculate the amount of times that it has been executed
             @Override
             public void visitPyIfStatement(PyIfStatement ifStatement) {
                 super.visitPyIfStatement(ifStatement);
 
-                ArrayList<Integer> partCounts = new ArrayList<>();
+                List<PyStatementPart> parts = Arrays.asList(
+                    ifStatement.getIfPart(),
+                    ifStatement.getElsePart()
+                );
+                parts.addAll(Arrays.asList(ifStatement.getElifParts()));
 
-                // TODO: This assumes that there exists an execution count on the first child of every branch
-                partCounts.add(feedbackWrapper.getExecutionCount(PythonBranchProbabilityProvider.getLineNumberOfChild(document, ifStatement.getIfPart(), PyStatementList.class)));
+                Map<Integer, Integer> partCounts = new HashMap<>();
 
-                for (PyIfPart part : ifStatement.getElifParts()) {
-                    partCounts.add(feedbackWrapper.getExecutionCount(PythonBranchProbabilityProvider.getLineNumberOfChild(document, part, PyStatementList.class)));
+                for (PyStatementPart part : parts) {
+                    if (part != null) {
+                        partCounts.put(textOffsetToLineNumber(document, part), getExecutionCountForElement(document, part, feedbackWrapper));
+                    }
                 }
 
-                if (ifStatement.getElsePart() != null) {
-                    partCounts.add(feedbackWrapper.getExecutionCount(PythonBranchProbabilityProvider.getLineNumberOfChild(document, ifStatement.getElsePart(), PyStatementList.class)));
+                int totalCount = partCounts.values().stream().mapToInt(i -> i).sum();
+
+                // If the else part is null, that means that we might never enter any branches of the if statement at all, and thus we should consider the
+                // probability relative to the parent of the if statement
+                if (ifStatement.getElsePart() == null) {
+                    PyElement parent = PsiTreeUtil.getParentOfType(ifStatement, PyElement.class);
+                    totalCount = getExecutionCountForElement(document, parent, feedbackWrapper);
                 }
 
-                int totalCount = partCounts.stream().mapToInt(i -> i).sum();
-
-                ifStatementTotalCount.put(ifStatement.getTextOffset(), totalCount);
+                // Compute the execution probability of each if statement branch as a fraction of the total execution count of all branches, as long as
+                // at least one of the branches was executed once
+                if (totalCount != 0) {
+                    for (Map.Entry<Integer, Integer> entry : partCounts.entrySet()) {
+                        executionProbabilities.put(entry.getKey(), entry.getValue() / (double) totalCount);
+                    }
+                }
             }
         });
-
-
-        for (int line : feedbackWrapper.getLineNumbers()) {
-            int offset = document.getLineStartOffset(line);
-            PsiElement element = pyFile.findElementAt(offset);
-            assert element != null;
-
-            element = PsiTreeUtil.findSiblingForward(element, PyElementTypes.STATEMENT_LIST, e -> {});
-
-            if (element == null) {
-                continue;
-            }
-
-            PyIfStatement ifStatement = PsiTreeUtil.getParentOfType(element, PyIfStatement.class);
-
-            if (ifStatement != null) {
-                executionProbabilities.put(line, feedbackWrapper.getExecutionCount(line) / (double) ifStatementTotalCount.get(ifStatement.getTextOffset()));
-            }
-        }
 
         return executionProbabilities;
     }
 
-    private static int getLineNumberOfChild(Document document, PsiElement element, Class<? extends PsiElement> elementClass) {
-        return document.getLineNumber(PsiTreeUtil.findChildOfType(element, elementClass).getTextOffset());
+    private static int textOffsetToLineNumber(Document document, PsiElement psiElement) {
+        return document.getLineNumber(psiElement.getTextOffset());
+    }
+
+    /**
+     * Finds the execution count of an element
+     * For most elements, the logic here is to take the maximum execution count of all children, because if a child executes X times, then the parent
+     * must also have executed that many times
+     * For certain other elements, such as if branches, we have to sum the execution counts of all the branches
+     * @param document For converting text offsets to line numbers
+     * @param pyElement The statement part to analyse
+     * @param fileFeedbackWrapper Feedback wrapper from which to get execution counts of lines
+     * @return Number of executions or 0 if the given element has no children of the type PyStatement
+     */
+    private static int getExecutionCountForElement(Document document, PyElement pyElement, FileFeedbackWrapper fileFeedbackWrapper) {
+        Collection<PyStatement> statements = PsiTreeUtil.findChildrenOfAnyType(pyElement, PyStatement.class);
+
+        OptionalInt max = statements.stream().mapToInt(s -> {
+            LOG.debug(s.getClass().getName());
+
+            if (s instanceof PyIfStatement) {
+                PyIfStatement ifStatement = (PyIfStatement) s;
+                return getExecutionCountForElement(document, ifStatement.getIfPart(), fileFeedbackWrapper)
+                    + getExecutionCountForElement(document, ifStatement.getElsePart(), fileFeedbackWrapper)
+                    + Arrays.stream(ifStatement.getElifParts()).mapToInt(elif -> getExecutionCountForElement(document, elif, fileFeedbackWrapper)).sum();
+            }
+
+            int lineNumber = document.getLineNumber(s.getTextOffset());
+            return fileFeedbackWrapper.getExecutionCount(lineNumber);
+        }).max();
+
+        return max.orElse(0);
     }
 
 }
