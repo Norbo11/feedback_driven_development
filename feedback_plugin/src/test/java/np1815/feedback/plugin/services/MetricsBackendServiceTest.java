1c30ae225bf3821515568975e467c0c7bb7d35dd
Norbert Podsadowski <norbo11@googlemail.com>
2019-05-08 15:53:21 +0100

    Commit various IntelliJ testing sources as grabbed from the intellij-community repository

--- feedback_plugin/src/test/java/np1815/feedback/plugin/services/MetricsBackendServiceTest.java
+++ feedback_plugin/src/test/java/np1815/feedback/plugin/services/MetricsBackendServiceTest.java
@@ -1,17 +1,10 @@
 package np1815.feedback.plugin.services;
 
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.roots.ContentEntry;
-import com.intellij.openapi.roots.ModifiableRootModel;
-import com.intellij.openapi.vcs.VcsApplicationSettings;
 import com.intellij.openapi.vcs.VcsException;
-import com.intellij.openapi.vcs.VcsRootSettings;
 import com.intellij.openapi.vcs.changes.Change;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.codeStyle.CodeStyleSettingsManager;
 import com.intellij.testFramework.LightProjectDescriptor;
 import com.intellij.testFramework.fixtures.*;
-import org.jetbrains.annotations.NotNull;
 import org.junit.Ignore;
 
 import java.util.Arrays;
@@ -21,6 +14,17 @@ import java.util.Map;
 
 // TODO: Fix this test not picking up that main.py is under VCS
 // Possibly need to use a heavy test, or set up a real temporary directory instead of the in-memory file system
+// hi I got feedback from git developers about writing tests
+// The correct way to write a VCS test is to extend the VcsPlatformTest (or GitPlatformTest if it is Git specific). It is a Heavy test indeed, but it is not
+// a big problem, since VCS tests won't benefit much from being transformed to Light tests. Inside the test one can generate Git repository as we usually do by firing correspondent commands (create file, modify, git add, git commit, etc.), or by copying a prepared .git folder: that doesn't matter, AFAIR the GitPlatformTest doesn't have any restrictions on that.
+// But please note that one can't depend on the vcs-tests module from a plugin right now, until
+// extends GitPlatformTest
+// NOTES:
+// - Can't depend on test sources, so need to get everything to compile first
+// - Need to handle copying from test data directory yourself unless we inline the files
+// - May need to refactor code to be testable
+// - Overall probably not worth it?
+
 @Ignore
 public class MetricsBackendServiceTest extends LightPlatformCodeInsightFixtureTestCase {
 
@@ -29,11 +33,14 @@ public class MetricsBackendServiceTest extends LightPlatformCodeInsightFixtureTe
     @Override
     public void setUp() throws Exception {
         super.setUp();
-        myFixture.copyDirectoryToProject(".git", ".git");
-        this.virtualFile = myFixture.copyFileToProject("main.py");
+//        myFixture.copyDirectoryToProject(".git", ".git");
+//        this.virtualFile = myFixture.copyFileToProject("main.py")
+//        testRoot.
+
 
 //        IdeaTestFixtureFactory.getFixtureFactory().createLightFixtureBuilder()
-//            IdeaTestExecutionPolicy.current().cr
+//            IdeaTestExecutionPolicy.current().c
+//        getTestRootFile().
     }
 
     @Override
@@ -44,7 +51,7 @@ public class MetricsBackendServiceTest extends LightPlatformCodeInsightFixtureTe
     public void testTranslateLinesAccordingToChanges() throws VcsException {
         MetricsBackendService service = new MetricsBackendService();
 
-        List<Change> changes = service.getChangesSinceVersion(myFixture.getProject(), virtualFile, "0e43d213d3048105fc2735356e21cff62c96afed");
+        List<Change> changes = service.getChangesSinceVersion(getProject(), virtualFile, "0e43d213d3048105fc2735356e21cff62c96afed");
 
         Map<Integer, TranslatedLineNumber> map = MetricsBackendServiceUtil.translateLinesAccordingToChanges(
             changes,
